0b0ca83cb523fccaa8b098ba59452dac8671874f
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-06 13:14:50 +0200

    Added heart logo to the profile picture

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ReactComponent as HeartLogo } from "../../assets/heart.svg";
 
 import "./Avatar.scss";
 
-export const Avatar = ({ imageSource }) => (
+export const Avatar = ({ imageSource, hasIcon }) => (
   <div className="profile">
     <img className="profile__picture" src={imageSource} alt="profile" />
+    {hasIcon && <HeartLogo className="profile__logo" />}
   </div>
 );
 
+Avatar.defaultProps = {
+  hasIcon: false,
+};
+
 Avatar.propTypes = {
   imageSource: PropTypes.string,
+  hasIcon: PropTypes.bool,
 };
