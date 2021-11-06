264969e37de2e741cc43f6529eb49d0a6d5f407e
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-06 14:52:10 +0200

    Created the skeleton of dropdown component

---
+++ src/components/DropDown/DropDown.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import "./DropDown.scss";
+
+export const DropDown = (props) => (
+  <div className="dropdown">
+    <div className="dropdown__item">Settings</div>
+    <div className="dropdown__item">Log out</div>
+  </div>
+);
+
+// DropDown.defaultProps = {};
+
+// DropDown.propTypes = {};
