87d65a8e85fe6eb050d1f3df8837ac73becbbe7f
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-08 21:29:25 +0200

    Removed redundant curly braces

--- src/components/atoms/DropDown/DropDown.jsx
+++ src/components/atoms/DropDown/DropDown.jsx
@@ -3,27 +3,25 @@ import PropTypes from "prop-types";
 
 import "./DropDown.scss";
 
-export const DropDown = ({ items }) => {
-  return (
-    <div className="drop-down">
-      {items.map((item) => {
-        return (
-          <div
-            key={item.id}
-            className="drop-down__item"
-            role="button"
-            tabIndex={item.id}
-            onClick={item.click}
-            onKeyDown={item.click}
-          >
-            <div className="drop-down__logo">{item.logo}</div>
-            <div>{item.text}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+export const DropDown = ({ items }) => (
+  <div className="drop-down">
+    {items.map((item) => {
+      return (
+        <div
+          key={item.id}
+          className="drop-down__item"
+          role="button"
+          tabIndex={item.id}
+          onClick={item.click}
+          onKeyDown={item.click}
+        >
+          <div className="drop-down__logo">{item.logo}</div>
+          <div>{item.text}</div>
+        </div>
+      );
+    })}
+  </div>
+);
 
 DropDown.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object),
