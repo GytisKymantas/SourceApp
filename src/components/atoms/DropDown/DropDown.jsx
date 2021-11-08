7b8938d016e209bfdacec67699f7605a05659d96
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-08 21:32:41 +0200

    Removed another redundant curly braces

--- src/components/atoms/DropDown/DropDown.jsx
+++ src/components/atoms/DropDown/DropDown.jsx
@@ -5,21 +5,19 @@ import "./DropDown.scss";
 
 export const DropDown = ({ items }) => (
   <div className="drop-down">
-    {items.map((item) => {
-      return (
-        <div
-          key={item.id}
-          className="drop-down__item"
-          role="button"
-          tabIndex={item.id}
-          onClick={item.click}
-          onKeyDown={item.click}
-        >
-          <div className="drop-down__logo">{item.logo}</div>
-          <div>{item.text}</div>
-        </div>
-      );
-    })}
+    {items.map((item) => (
+      <div
+        key={item.id}
+        className="drop-down__item"
+        role="button"
+        tabIndex={item.id}
+        onClick={item.click}
+        onKeyDown={item.click}
+      >
+        <div className="drop-down__logo">{item.logo}</div>
+        <div>{item.text}</div>
+      </div>
+    ))}
   </div>
 );
 
