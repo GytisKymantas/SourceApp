de0f8f8bc592ca8cb8d85fabd01e78662172542a
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-13 19:44:11 +0200

    Added notification icon and some styling

--- src/components/molecules/ProfileWidget/ProfileWidget.jsx
+++ src/components/molecules/ProfileWidget/ProfileWidget.jsx
@@ -1,12 +1,23 @@
 import { Avatar } from "components/atoms/Avatar/Avatar";
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as Notification } from "../../../assets/notification.svg";
+import { ReactComponent as Elipse } from "../../../assets/elipse.svg";
 // import PropTypes from "prop-types";
 
 import "./ProfileWidget.scss";
 
 export const ProfileWidget = () => {
+  // state placeholder for future notifications functionality,
+  // suppose to be false by default actually
+  // eslint-disable-next-line no-unused-vars
+  const [notifications, setNotifications] = useState(true);
+
   return (
     <div className="profile-widget">
+      <div className="profile-widget__notifications-container">
+        <Notification className="profile-widget__notification-icon" />
+        {notifications && <Elipse className="profile-widget__elipse-icon" />}
+      </div>
       <div className="profile-widget__avatar">
         <Avatar
           imageSource={
@@ -15,7 +26,7 @@ export const ProfileWidget = () => {
           hasIcon={true}
         />
       </div>
-      <div className="profile-widget__something"></div>
+      {/* <div className="profile-widget__something"></div> */}
     </div>
   );
 };
