a2e3890df901046618239977ba89f78c778a4f5b
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-11-14 18:40:26 +0200

    Header skeleton

---
+++ src/components/molecules/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./header.scss";
+
+export const Header = () => (
+  <header className="header">
+    <div className="header__logo">{/* placeholder for logo */}</div>
+    <div className="header__nav">{/* placeholder for navigation */}</div>
+    <div className="header__notification-bell">
+      {/* placeholder for notification bell */}
+    </div>
+    <div className="header__user-profile-widget">
+      {/* placeholder for user profile widget */}
+    </div>
+  </header>
+);
