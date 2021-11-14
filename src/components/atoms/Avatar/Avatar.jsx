9b5d2eb24d0bb7e1844ac36759146fadf0b2f42a
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-14 22:21:09 +0200

    Added default image to profile if no image source is provided

--- src/components/atoms/Avatar/Avatar.jsx
+++ src/components/atoms/Avatar/Avatar.jsx
@@ -4,7 +4,10 @@ import { ReactComponent as HeartLogo } from "../../../assets/heart.svg";
 
 import "./Avatar.scss";
 
-export const Avatar = ({ imageSource, hasIcon }) => (
+export const Avatar = ({
+  imageSource = "https://thumbs.dreamstime.com/b/default-avatar-profile-trendy-style-social-media-user-icon-187599373.jpg",
+  hasIcon,
+}) => (
   <div className="avatar">
     <img className="avatar__picture" src={imageSource} alt="profile" />
     {hasIcon && <HeartLogo className="avatar__logo" />}
