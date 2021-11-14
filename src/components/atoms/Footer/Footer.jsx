b4d53fe7cea3eb944b2879d0a26aed1e8328bfbe
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-11-14 14:30:01 +0200

    footer styles

---
+++ src/components/atoms/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./footer.scss";
+
+export const Footer = () => (
+  <footer className="footer">
+    <span className="footer__text">Copyright © 2021 Devbridge</span>
+  </footer>
+);
