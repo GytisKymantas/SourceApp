a1789ca7200ca2bd23ab2af7cea6bdebdc5edac5
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-15 22:21:57 +0200

    added responsive parameters

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -4,14 +4,12 @@ import "./menu-item.scss";
 import { Link } from "react-router-dom";
 
 export const MenuItem = ({ icon, name, link }) => (
-  <div className="menu-item">
-    <Link to={link}>
-      <div className="menu-item__content">
-        <div className="menu-item__icon">{icon}</div>
-        <div className="menu-item__name">{name}</div>
-      </div>
-    </Link>
-  </div>
+  <Link to={link} className="menu-item">
+    <div className="menu-item__content">
+      <div className="menu-item__icon">{icon}</div>
+      <div className="menu-item__name">{name}</div>
+    </div>
+  </Link>
 );
 MenuItem.propTypes = {
   icon: PropTypes.node,
