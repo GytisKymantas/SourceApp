8097eb9ccc1ee51708a7060d9d9cea46e778a54d
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-11-15 20:09:12 +0200

    Created skeleton component with required index files

---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import "./Registration.scss";
+
+export const Registration = () => {
+  return (
+    <div className="registration">
+      <div>Skeleton</div>
+    </div>
+  );
+};
+
+// Registration.defaultProps = {
+// };
+
+// Registration.propTypes = {
+// };
