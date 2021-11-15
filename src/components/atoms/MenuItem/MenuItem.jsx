d575d88d862bf16a469b16d3799dcdf25f91cba2
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-15 18:18:24 +0200

    deleted return with curly braces

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -3,18 +3,16 @@ import PropTypes from "prop-types";
 import "./menu-item.scss";
 import { Link } from "react-router-dom";
 
-export const MenuItem = ({ icon, name, link }) => {
-  return (
-    <div className="menuItem">
-      <Link to={link}>
-        <div className="menuItem__content">
-          <div className="menuItem__icon">{icon}</div>
-          <div className="menuItem__name">{name}</div>
-        </div>
-      </Link>
-    </div>
-  );
-};
+export const MenuItem = ({ icon, name, link }) => (
+  <div className="menuItem">
+    <Link to={link}>
+      <div className="menuItem__content">
+        <div className="menuItem__icon">{icon}</div>
+        <div className="menuItem__name">{name}</div>
+      </div>
+    </Link>
+  </div>
+);
 MenuItem.propTypes = {
   icon: PropTypes.node,
   name: PropTypes.string,
