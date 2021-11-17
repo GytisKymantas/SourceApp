4f1e96aa7bde7986622be6692eb0a2af1fdf8e07
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-17 14:05:05 +0200

    added activeClass parameter to menu-item component

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./menu-item.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const MenuItem = ({ icon, name, link }) => (
-  <Link to={link} className="menu-item">
+  <NavLink to={link} className="menu-item" activeClassName="active">
     <div className="menu-item__content">
       <div className="menu-item__icon">{icon}</div>
       <div className="menu-item__name">{name}</div>
     </div>
-  </Link>
+  </NavLink>
 );
 MenuItem.propTypes = {
   icon: PropTypes.node,
