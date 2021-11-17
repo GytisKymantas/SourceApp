5c8085f18ddf2d0da30c8029efe48126c9cb90c2
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-17 13:55:11 +0200

    added router code to App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { ReactComponent as SourceryLogo } from "assets/logo.svg";
 import { GetStartedList } from "features/getStarted/components/GetStartedList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigation } from "components/molecules/Navigation/Navigation";
+import { Dashboard } from "pages/Dashboard";
+import { Reservations } from "pages/Reservations";
+import { EatOut } from "pages/EatOut";
+import { DropDown } from "components/atoms/DropDown/DropDown";
+import { ReactComponent as SettingsLogo } from "./assets/settings.svg";
+import { ReactComponent as LogOutLogo } from "./assets/logOut.svg";
 
 function App() {
   const [instructions, setInstructions] = useState([]);
@@ -17,11 +24,40 @@ function App() {
         }
       );
   }, []);
+  const menuItems = [
+    {
+      id: 1,
+      logo: <SettingsLogo />,
+      text: "Settings",
+    },
+    {
+      id: 2,
+      logo: <LogOutLogo />,
+      text: "Log out",
+    },
+  ];
 
+  const handleOptionSelect = (optionId) => {
+    // placeholder for future functionality
+    // eslint-disable-next-line no-console
+    console.log("option selected", optionId);
+  };
   return (
     <div className="app">
       <header className="App-header">
-        <SourceryLogo />
+        <Router>
+          <div className="app">
+            <DropDown options={menuItems} onSelect={handleOptionSelect} />
+            <Navigation />
+            <div className="content">
+              <Routes>
+                <Route exact path="/" element={<Dashboard />} />
+                <Route path="/reservations" element={<Reservations />} />
+                <Route path="/eatOut" element={<EatOut />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
