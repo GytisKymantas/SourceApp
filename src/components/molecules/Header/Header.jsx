caacb73ac14d0764dd2ade637c422f0df4741f6e
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-11-19 16:28:05 +0200

    updated header component according to PR comments

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { ReactComponent as Logo } from "assets/rocketLogo.svg";
-import { ReactComponent as Notification } from "../../../assets/notification.svg";
 
 import "./header.scss";
+import { ProfileWidget } from "../ProfileWidget/ProfileWidget";
 
 export const Header = () => (
   <header className="header">
     <Logo className="header__logo" />
-    <div className="header__nav"></div>
-    <div className="header__user-profile-container">
-      <Notification className="header__notification-bell" />
-      <div className="header__user-profile-widget"></div>
-    </div>
+    <ProfileWidget className="header__user-profile" />
   </header>
 );
