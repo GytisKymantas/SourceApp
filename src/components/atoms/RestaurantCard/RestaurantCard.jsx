31b462094493218e3623235b3e584d08770492b5
gytis <Gytis.kymantas@yahoo.com>
2021-11-22 20:28:24 +0200

    second commit

--- src/components/atoms/RestaurantCard/RestaurantCard.jsx
+++ src/components/atoms/RestaurantCard/RestaurantCard.jsx
@@ -3,11 +3,11 @@ import "./RestaurantCard.scss";
 import PropTypes from "prop-types";
 
 const RestaurantCard = ({headerImage,userImage,vectorImage,heartImage}) => (
-  <div className="restaurant__container">
-      <div className="restaurant__container-front">
+  <div className="restaurant">
+      <div className="restaurant__front">
           <img src={userImage} alt="user"></img>
           <img src={headerImage} alt="pizza"></img>
-      <div className="restaurant__container-frame">
+      <div className="restaurant__frame">
           <img src={vectorImage} alt="vector"></img>
           <span>4.5</span>
       </div>
@@ -17,10 +17,10 @@ const RestaurantCard = ({headerImage,userImage,vectorImage,heartImage}) => (
           <li>Snacks</li>
           <li>Pizza</li>
       </ul>
-      <div className="restaurant__container-image-heart">
+      <div className="restaurant__image-heart">
           <img src={heartImage} alt="heart"></img>
       </div>
-      <div className="restaurant__container-image-text">
+      <div className="restaurant__image-text">
           <h2>Ristorante Viva Piccola it...</h2>
           <span>10:00 - 21:00</span>
       </div>
