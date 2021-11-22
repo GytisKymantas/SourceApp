a864f83e5cf011a4b5c563774b13a4b25c677b03
gytis <Gytis.kymantas@yahoo.com>
2021-11-22 12:16:55 +0200

    prop name changes

--- src/components/atoms/Reservations/ReservationsItem.jsx
+++ src/components/atoms/Reservations/ReservationsItem.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import "./Reservations.scss";
 import PropTypes from "prop-types";
-import logo from "../../../assets/door.svg";
 
-const ReservationsItem = ({ headerName, reservedNumber }) => (
+const ReservationsItem = ({
+  reservationName,
+  reservationImage,
+  reservationNumber,
+}) => (
   <div className="reservations__container">
     <div className="reservations__container-header">
-      <h1>{headerName}</h1>
-      <p>{reservedNumber ? `${reservedNumber} RESERVED` : "UNRESERVED"}</p>
+      <h1>{reservationName}</h1>
+      <p>
+        {reservationNumber ? `${reservationNumber} RESERVED` : "UNRESERVED"}
+      </p>
     </div>
     <div className="reservations__container-door">
       <div className="reservations__container-doorimg door">
-        <img src={logo} alt="logo"></img>
+        <img src={reservationImage} alt="logo"></img>
       </div>
     </div>
   </div>
 );
 
 ReservationsItem.propTypes = {
-  headerImage: PropTypes.string,
-  headerName: PropTypes.string,
-  reservedNumber: PropTypes.number,
+  reservationImage: PropTypes.string,
+  reservationName: PropTypes.string,
+  reservationNumber: PropTypes.number,
 };
 
 export default ReservationsItem;
