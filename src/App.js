2afee61717fc19b3810cf29ff39fbd7a4ba7a968
Vitoldas <Vitoldascebatorius@gmail.com>
2021-11-24 11:41:33 +0200

    Cleared app.js conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from "react";
-// import { ReactComponent as SourceryLogo } from "assets/logo.svg";
-// import { GetStartedList } from "features/getStarted/components/GetStartedList";
-import News from "../src/components/newFeedItem/News";
+import React, { useEffect, useState } from "react";
+import { ReactComponent as SourceryLogo } from "assets/logo.svg";
+import { GetStartedList } from "features/getStarted/components/GetStartedList";
 
 function App() {
+  const [instructions, setInstructions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3008/instructions")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setInstructions(result);
+        },
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
+
   return (
     <div className="app">
-      <News></News>
+      <header className="App-header">
+        <SourceryLogo />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <GetStartedList key={instructions.length} instructions={instructions} />
     </div>
   );
 }
