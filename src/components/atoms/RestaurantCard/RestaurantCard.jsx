408375c3df0f58e3c676fb28c9fea508ec4b3061
gytis <Gytis.kymantas@yahoo.com>
2021-11-24 20:06:23 +0200

    added photos, if statement, styling

--- src/components/atoms/RestaurantCard/RestaurantCard.jsx
+++ src/components/atoms/RestaurantCard/RestaurantCard.jsx
@@ -1,32 +1,72 @@
 import React from "react";
 import "./RestaurantCard.scss";
 import PropTypes from "prop-types";
+import globe from "./globe.svg";
+import map from "./map.svg";
+import redheart from "./redheart copy.svg";
 
-const RestaurantCard = ({headerImage,userImage,vectorImage,heartImage}) => (
+export const RestaurantCard = ({
+  headerImage,
+  userImage,
+  vectorImage,
+  heartImage,
+  isTrue,
+}) => (
   <div className="restaurant">
-      <div className="restaurant__front">
-          <img src={ userImage } alt="user"></img>
-          <img src={ headerImage } alt="pizza"></img>
+    <div className="restaurant__front">
+      <img src={userImage} alt="user" className="user" />
+      <img src={headerImage} alt="pizza"></img>
       <div className="restaurant__frame">
-          <img src={ vectorImage } alt="vector"></img>
-          <span>4.5</span>
+        <img src={vectorImage} alt="vector"></img>
+        <span>4.5</span>
       </div>
-
       <ul>
-          <li>Salads</li>
-          <li>Snacks</li>
-          <li>Pizza</li>
+        <li>Salads</li>
+        <li>Snacks</li>
+        <li>Pizza</li>
       </ul>
       <div className="restaurant__image-heart">
-          <img src={heartImage} alt="heart"></img>
+        {isTrue ? (
+          <img src={redheart} alt="redheart" />
+        ) : (
+          <img src={heartImage} alt="heart" />
+        )}
       </div>
       <div className="restaurant__image-text">
-          <h2>Ristorante Viva Piccola it...</h2>
-          <span>10:00 - 21:00</span>
+        <h2>Ristorante Viva Piccola it...</h2>
+        <span>10:00 - 21:00</span>
       </div>
+      {isTrue ? (
+        <div className="restaurant__hidden">
+          <div className="restaurant__examples">
+            <span>
+              <img src={globe} alt="globe"></img>
+              EXAMPLE.COM
+            </span>
+            <span>
+              <img src={map} alt="map"></img>
+              EXAMPLE ST.32
+            </span>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum
+            dolor sit amet.Lorem ipsum dolor sit amet.
+          </p>
+          <div className="restaurant__buttons">
+            <button>READ MORE</button>
+            <button>CHECK-IN</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   </div>
-</div>
-
-)
-
-export default RestaurantCard;
+);
+RestaurantCard.propTypes = {
+  headerImage: PropTypes.string,
+  userImage: PropTypes.string,
+  vectorImage: PropTypes.string,
+  heartImage: PropTypes.string,
+  isTrue: PropTypes.string,
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { RestaurantCard } from "components/atoms/RestaurantCard/RestaurantCard";
+import pizza from "components/atoms/RestaurantCard/Photo copy.jpg";
+import user from "components/atoms/RestaurantCard/Check in copy.svg";
+import vector from "components/atoms/RestaurantCard/Vector copy.svg";
+import heart from "components/atoms/RestaurantCard/heart copy.svg";
 
 export const Dashboard = () => (
   <div>
     <h1>Dashboard</h1>
-    <RestaurantCard />
+    <RestaurantCard
+      headerImage={pizza}
+      userImage={user}
+      heartImage={heart}
+      vectorImage={vector}
+      isTrue={false}
+    />
   </div>
 );
