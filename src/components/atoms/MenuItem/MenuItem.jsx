f3ae68026a4293e0605b4216408d2206dd8ad26f
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-25 15:33:49 +0200

    fixed activeclassname error

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ import "./menu-item.scss";
 import { NavLink } from "react-router-dom";
 
 export const MenuItem = ({ icon, name, link }) => (
-  <NavLink to={link} className="menu-item" activeClassName="active">
+  <NavLink to={link} className="menu-item" activeclassname="active">
     <div className="menu-item__content">
       <div className="menu-item__icon">{icon}</div>
       <div className="menu-item__name">{name}</div>
