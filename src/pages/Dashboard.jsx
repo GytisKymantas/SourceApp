762e174659c128e90f0f061aa0e0077516df126c
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-29 23:24:19 +0200

    added hellowidget to dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,9 @@
+import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import React from "react";
-import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 
-export const Dashboard = () => {
-  return (
-    <div>
-      <HelloWidget />
-    </div>
-  );
-};
+export const Dashboard = () => (
+  <MainLayout>
+    <HelloWidget />
+  </MainLayout>
+);
