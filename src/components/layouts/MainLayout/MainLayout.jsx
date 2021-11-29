5691cbe93a68aa990241b822ff58a106d1ef5af0
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-11-29 09:56:30 +0200

    MainLayout moved from App.js to separate pages

--- src/components/layouts/MainLayout/MainLayout.jsx
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import { Header } from "components/organisms/Header/Header";
 import { Footer } from "components/atoms/Footer/Footer";
 import { PropTypes } from "prop-types";
-import React from "react";
+import { Breadcrumbs } from "components/atoms/Breadcrumbs/Breadcrumbs";
+
 import "./main-layout.scss";
 
 export const MainLayout = ({ children }) => (
   <>
     <Header />
     <main>
+      <Breadcrumbs />
       <div className="content">{children}</div>
     </main>
     <Footer />
