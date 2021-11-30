ab0946b1dd33980b599cb9da4409ca23c8e00dfb
Vitoldas <Vitoldascebatorius@gmail.com>
2021-11-30 18:09:23 +0200

    updated branch to be close to dev

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { ReactComponent as SourceryLogo } from "assets/logo.svg";
-import { GetStartedList } from "features/getStarted/components/GetStartedList";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "pages/Dashboard";
+import { Reservations } from "pages/Reservations";
+import { EatOut } from "pages/EatOut";
+import { NotFound } from "pages/NotFound";
 
 function App() {
-  const [instructions, setInstructions] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3008/instructions")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setInstructions(result);
-        },
-        (error) => {
-          // handle error here
-        }
-      );
-  }, []);
-
   return (
     <div className="app">
-      <header className="App-header">
-        <SourceryLogo />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <GetStartedList key={instructions.length} instructions={instructions} />
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/reservations" element={<Reservations />} />
+          <Route
+            path="/dashboard/reservations/meetingrooms"
+            element={<Reservations />}
+          />
+          <Route
+            path="/dashboard/reservations/books"
+            element={<Reservations />}
+          />
+          <Route
+            path="/dashboard/reservations/devices"
+            element={<Reservations />}
+          />
+          <Route path="/dashboard/eatout" element={<EatOut />} />
+          <Route path="/dashboard/eatout/category" element={<EatOut />} />
+          <Route
+            path="/dashboard/eatout/category/single"
+            element={<EatOut />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
