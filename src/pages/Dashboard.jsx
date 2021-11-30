981f46af4377bf06c448cf1b903ace79092c1b38
gytis <Gytis.kymantas@yahoo.com>
2021-11-30 15:37:04 +0200

    fixed dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,8 @@
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
-import { ReservationsSection } from "components/molecules/ReservationsSection/ReservationsSection";
 import React from "react";
 
 export const Dashboard = () => (
   <MainLayout>
     <h1>Dashboard</h1>
-    <ReservationsSection />
   </MainLayout>
 );
