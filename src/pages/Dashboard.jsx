986fd357a69ddf77832c0bd69701087eec5976bc
gytis <Gytis.kymantas@yahoo.com>
2021-11-30 20:22:40 +0200

    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,8 @@
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import React from "react";
-import { RestaurantCard } from "components/atoms/RestaurantCard/RestaurantCard";
-import pizza from "components/atoms/RestaurantCard/Photo copy.jpg";
-import user from "components/atoms/RestaurantCard/Check in copy.svg";
-import vector from "components/atoms/RestaurantCard/Vector copy.svg";
-import heart from "components/atoms/RestaurantCard/heart copy.svg";
 
 export const Dashboard = () => (
   <MainLayout>
     <h1>Dashboard</h1>
-<<<<<<< HEAD
   </MainLayout>
-=======
-    <RestaurantCard
-      headerImage={pizza}
-      userImage={user}
-      heartImage={heart}
-      vectorImage={vector}
-      isTrue={false}
-    />
-  </div>
->>>>>>> feature/SFE21R-42-RestaurantCard
 );
