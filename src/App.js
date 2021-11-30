49c5905fd92d75f8d2fd5bbdec821d1b71bb2c70
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-11-30 18:06:38 -0500

    implemented user data fetching

--- src/App.js
+++ src/App.js
@@ -4,37 +4,59 @@ import { Dashboard } from "pages/Dashboard";
 import { Reservations } from "pages/Reservations";
 import { EatOut } from "pages/EatOut";
 import { NotFound } from "pages/NotFound";
-
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/reservations" element={<Reservations />} />
-          <Route
-            path="/dashboard/reservations/meetingrooms"
-            element={<Reservations />}
-          />
-          <Route
-            path="/dashboard/reservations/books"
-            element={<Reservations />}
-          />
-          <Route
-            path="/dashboard/reservations/devices"
-            element={<Reservations />}
-          />
-          <Route path="/dashboard/eatout" element={<EatOut />} />
-          <Route path="/dashboard/eatout/category" element={<EatOut />} />
-          <Route
-            path="/dashboard/eatout/category/single"
-            element={<EatOut />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/userData.json"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        },
+
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
+
+  if (data) {
+    return (
+      <div className="app">
+        <Router>
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard data={data} />} />
+            <Route path="/dashboard/reservations" element={<Reservations />} />
+            <Route
+              path="/dashboard/reservations/meetingrooms"
+              element={<Reservations />}
+            />
+            <Route
+              path="/dashboard/reservations/books"
+              element={<Reservations />}
+            />
+            <Route
+              path="/dashboard/reservations/devices"
+              element={<Reservations />}
+            />
+            <Route path="/dashboard/eatout" element={<EatOut />} />
+            <Route path="/dashboard/eatout/category" element={<EatOut />} />
+            <Route
+              path="/dashboard/eatout/category/single"
+              element={<EatOut />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  } else {
+    return "Loading...";
+  }
 }
 
 export default App;
