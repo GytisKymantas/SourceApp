26df42443fe5001231a18eec2ccfd8ce9d63f44e
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-01 11:31:51 +0200

    added app.js file

--- src/App.js
+++ src/App.js
@@ -25,32 +25,39 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard data={data} />} />
-          <Route path="/dashboard/reservations" element={<Reservations />} />
-          <Route
-            path="/dashboard/reservations/meetingrooms"
-            element={<Reservations />}
-          />
-          <Route
-            path="/dashboard/reservations/books"
-            element={<Reservations />}
-          />
-          <Route
-            path="/dashboard/reservations/devices"
-            element={<Reservations />}
-          />
-          <Route path="/dashboard/eatout" element={<EatOut />} />
-          <Route path="/dashboard/eatout/category" element={<EatOut />} />
-          <Route
-            path="/dashboard/eatout/category/single"
-            element={<EatOut />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+    <div>
+      {data && (
+        <div className="app">
+          <Router>
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard data={data} />} />
+              <Route
+                path="/dashboard/reservations"
+                element={<Reservations />}
+              />
+              <Route
+                path="/dashboard/reservations/meetingrooms"
+                element={<Reservations />}
+              />
+              <Route
+                path="/dashboard/reservations/books"
+                element={<Reservations />}
+              />
+              <Route
+                path="/dashboard/reservations/devices"
+                element={<Reservations />}
+              />
+              <Route path="/dashboard/eatout" element={<EatOut />} />
+              <Route path="/dashboard/eatout/category" element={<EatOut />} />
+              <Route
+                path="/dashboard/eatout/category/single"
+                element={<EatOut />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
