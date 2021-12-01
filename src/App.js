a241e5e7caa0507cdca43084f500a5f0113284b2
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-01 12:52:24 +0200

    changed validation position

--- src/App.js
+++ src/App.js
@@ -25,39 +25,35 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {data && (
-        <div className="app">
-          <Router>
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard data={data} />} />
-              <Route
-                path="/dashboard/reservations"
-                element={<Reservations />}
-              />
-              <Route
-                path="/dashboard/reservations/meetingrooms"
-                element={<Reservations />}
-              />
-              <Route
-                path="/dashboard/reservations/books"
-                element={<Reservations />}
-              />
-              <Route
-                path="/dashboard/reservations/devices"
-                element={<Reservations />}
-              />
-              <Route path="/dashboard/eatout" element={<EatOut />} />
-              <Route path="/dashboard/eatout/category" element={<EatOut />} />
-              <Route
-                path="/dashboard/eatout/category/single"
-                element={<EatOut />}
-              />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Router>
-        </div>
-      )}
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={data ? <Dashboard data={data} /> : "loading"}
+          />
+          <Route path="/dashboard/reservations" element={<Reservations />} />
+          <Route
+            path="/dashboard/reservations/meetingrooms"
+            element={<Reservations />}
+          />
+          <Route
+            path="/dashboard/reservations/books"
+            element={<Reservations />}
+          />
+          <Route
+            path="/dashboard/reservations/devices"
+            element={<Reservations />}
+          />
+          <Route path="/dashboard/eatout" element={<EatOut />} />
+          <Route path="/dashboard/eatout/category" element={<EatOut />} />
+          <Route
+            path="/dashboard/eatout/category/single"
+            element={<EatOut />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
