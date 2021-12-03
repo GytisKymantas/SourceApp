1ecf2cc6f5694751eacc4494f3ca4ccfdb3bd7d7
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-03 15:13:16 +0200

    implemented stories data fetching

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,36 @@
 import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import React from "react";
-import PropTypes from "prop-types";
-
+import { useState, useEffect } from "react";
+import { StoriesSection } from "components/organisms/StoriesSection/StoriesSection";
 export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
   const firstName = userData?.userName.split(" ")[0];
+  const [storiesData, setStoriesData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/stories.json"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setStoriesData(result);
+        },
+
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
+  const Birthdays = storiesData?.stories.filter((x) => x.type === "birthday");
+  const Posts = storiesData?.stories.filter((x) => x.type === "post");
+  const Videos = storiesData?.stories.filter((x) => x.type === "video");
+
   return (
     <MainLayout>
       <HelloWidget name={firstName} />
+      <StoriesSection birthdays={Birthdays} posts={Posts} videos={Videos} />
     </MainLayout>
   );
 };
