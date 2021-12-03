602ad6e7df7b0e72c1a38ec381bf64124c3e9da4
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-03 23:43:01 +0200

    added some styles

--- src/components/atoms/WeatherWidget/WeatherWidget.jsx
+++ src/components/atoms/WeatherWidget/WeatherWidget.jsx
@@ -5,19 +5,23 @@ import { ReactComponent as Snow } from "../../../assets/weatherSnow.svg";
 import { ReactComponent as Thunderstorm } from "../../../assets/weatherThunderstorm.svg";
 import { ReactComponent as Sun } from "../../../assets/weatherSunny.svg";
 import { ReactComponent as Cloud } from "../../../assets/weatherCloudy.svg";
+import "./weather-widget.scss";
 
 export const WeatherWidget = () => {
   const api = "9b176ea0bfec0899a9f8b1d8250ffe11";
   const [weatherData, setWeatherData] = useState(null);
-  let lat = 0;
-  let long = 0;
+  let lat,
+    long = 0;
+  let weatherId,
+    windSpeed,
+    humidity,
+    weather = null;
+
   navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
     long = position.coords.longitude;
   });
-  let weatherId = null;
-  let windSpeed = null;
-  let humidity = null;
+
   useEffect(() => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`
@@ -33,37 +37,49 @@ export const WeatherWidget = () => {
         }
       );
   }, [lat, long]);
+
   weatherId = weatherData?.weather[0].id;
   windSpeed = weatherData?.wind.speed;
   humidity = weatherData?.main.humidity;
+
+  weatherId > 199 && weatherId < 300
+    ? (weather = "Thunderstorm")
+    : weatherId > 299 && weatherId < 600
+    ? (weather = "Rain")
+    : weatherId > 599 && weatherId < 700
+    ? (weather = "Snow")
+    : weatherId > 799 && weatherId < 900
+    ? (weather = "Cloudy")
+    : (weather = "Sunny");
+
   return (
     <div>
       {weatherData ? (
-        <div>
-          <p>City Name: {weatherData?.name}</p>
-          <p>
-            Temprature: {Math.round(weatherData?.main.temp)}
+        <div className="weather-data">
+          <p className="weather-data__city">{weatherData?.name}</p>
+          <p className="weather-data__temperature">
+            {Math.round(weatherData?.main.temp)}
             {"\u00b0"}
           </p>
-          {weatherId > 199 && weatherId < 300 ? (
-            <Thunderstorm />
-          ) : weatherId > 299 && weatherId.id < 600 ? (
-            <Rain />
-          ) : weatherId > 599 && weatherId < 700 ? (
-            <Snow />
-          ) : weatherId > 799 && weatherId < 900 ? (
-            <Cloud />
-          ) : (
-            <Sun />
-          )}
-          <p>ID:{weatherData.weather[0].id}</p>
-          <p>Wind speed: {windSpeed} m/s</p>
-          <p>Humidity: {humidity} mm</p>
-
-          <p>Description: {weatherData.weather[0].description}</p>
+          <p className="weather-data__icon">
+            {weather === "Sunny" ? (
+              <Sun />
+            ) : weather === "Rain" ? (
+              <Rain />
+            ) : weather === "Snow" ? (
+              <Snow />
+            ) : weather === "Cloudy" ? (
+              <Cloud />
+            ) : (
+              <Thunderstorm />
+            )}
+          </p>
+          <p className="weather-data__wind-speed">{windSpeed} m/s</p>
+          <p className="weather-data__humidity">{humidity} mm</p>
+          <p className="weather-data__description">{weather}</p>
         </div>
       ) : (
-        "Loading"
+        "Loading..."
       )}
     </div>
   );
