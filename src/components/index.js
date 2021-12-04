d4b10a346795ac3d7215d8d66314514eae1de7b8
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-12-04 20:03:27 +0200

    Updated index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
 // ----- ATOMS ----- //
 export * from "./atoms/Avatar/index";
 export * from "./atoms/DropDown/index";
-export * from "./atoms/StartingPageLayout/index";
 export * from "./atoms/MenuItem/index";
 
 // ----- MOLECULES ----- //
 export * from "./molecules/ProfileWidget/index";
 export * from "./molecules/RegistrationPage/index";
 export * from "./molecules/Navigation/index";
+
+// ----- ORGANISMS ----- //
+
+// ----- LAYOUTS ----- //
+export * from "./layouts/StartingPageLayout/index";
