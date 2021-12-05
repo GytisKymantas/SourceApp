fac4ffe0a0fff26c9410f8fd58bce203108462e2
gytis <Gytis.kymantas@yahoo.com>
2021-12-05 16:57:49 +0200

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,16 @@
 import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import React from "react";
-<<<<<<< HEAD
-import { ReservationsSection } from "components/molecules/ReservationsSection/ReservationsSection";
-
-export const Dashboard = () => (
-  <MainLayout>
-    <ReservationsSection />
-    <h1>Dashboard</h1>
-  </MainLayout>
-);
-=======
 import PropTypes from "prop-types";
+import { ReservationsSection } from "components/molecules/ReservationsSection/ReservationsSection";
 
 export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
   const firstName = userData?.userName.split(" ")[0];
   return (
     <MainLayout>
+      <ReservationsSection />
       <HelloWidget name={firstName} />
     </MainLayout>
   );
 };
->>>>>>> dev
