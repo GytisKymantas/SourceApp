a984cb0b06e67ba0c03568190c35bc988f586d80
gytis <Gytis.kymantas@yahoo.com>
2021-12-07 14:09:31 +0200

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,8 @@ import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import { ReservationsSection } from "components/molecules/ReservationsSection/ReservationsSection";
 import React from "react";
-<<<<<<< HEAD
-=======
 import { useState, useEffect } from "react";
 import { StoriesSection } from "components/organisms/StoriesSection/StoriesSection";
->>>>>>> dev
 
 export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
