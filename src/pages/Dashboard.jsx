4cac9bf02b19fde9c060c3473cd972873f7466a1
gytis <Gytis.kymantas@yahoo.com>
2021-12-07 14:12:15 +0200

    changes on conflict

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,11 +9,30 @@ export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
   const firstName = userData?.userName.split(" ")[0];
   const reservationInfo = userData?.reservations;
+  const [storiesData, setStoriesData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/stories.json"
+    )
+      .then((res) => res.json())
+
+      .then(
+        (result) => {
+          setStoriesData(result);
+        },
+
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
 
   return (
     <MainLayout>
       <ReservationsSection info={reservationInfo} />
       <HelloWidget name={firstName} />
+      {storiesData && <StoriesSection data={storiesData} />}
     </MainLayout>
   );
 };
