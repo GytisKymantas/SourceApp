29b7a04551e544cdff0e75f311cee9747756d44c
gytis <Gytis.kymantas@yahoo.com>
2021-12-07 16:17:10 +0200

    BEM methodology changes

---
+++ src/components/atoms/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./restaurant-card.scss";
+import PropTypes from "prop-types";
+
+export const RestaurantCard = ({
+  headerImage,
+  userImage,
+  vectorImage,
+  heartImage,
+}) => (
+  <div className="restaurant__container">
+    <div className="restaurant__container-front">
+      <img src={userImage} alt="user" />
+      <img src={headerImage} alt="pizza" />
+      <div className="restaurant__container-frame">
+        <img src={vectorImage} alt="vector" />
+        <span>4.5</span>
+      </div>
+      <ul>
+        <li>Salads</li>
+        <li>Snacks</li>
+        <li>Pizza</li>
+      </ul>
+      <div className="restaurant__container-image-heart">
+        <img src={heartImage} alt="heart" />
+      </div>
+      <div className="restaurant__container-image-text">
+        <h2>Ristorante Viva Piccola it...</h2>
+        <span>10:00 - 21:00</span>
+      </div>
+    </div>
+    <div className="restaurant__container-hidden">
+      <div className="restaurant__container-examples">
+        <span>
+          <img src={globe} alt="globe" />
+          EXAMPLE.COM
+        </span>
+        <span>
+          <img src={map} alt="map" />
+          EXAMPLE ST.32
+        </span>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor
+        sit amet.Lorem ipsum dolor sit amet.
+      </p>
+      <div className="restaurant__container-buttons">
+        <button>READ MORE</button>
+        <button>CHECK-IN</button>
+      </div>
+    </div>
+  </div>
+);
