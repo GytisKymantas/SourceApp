2764bb05844fc54b443f40a32e952d8794e5cbc8
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-09 12:26:42 +0200

    changes timer place from Hello widget to Greeting section

--- src/components/molecules/GreetingSection/GreetingSection.jsx
+++ src/components/molecules/GreetingSection/GreetingSection.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { WeatherWidget } from "components/atoms/WeatherWidget/WeatherWidget";
 import { HelloWidget } from "components/atoms/HelloWidget/HelloWidget";
 import "./greeting-section.scss";
 import PropTypes from "prop-types";
 
 export const GreetingSection = ({ name }) => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="greeting-section">
-      <HelloWidget name={name} className="greeting-section__hello-widget" />
-      <WeatherWidget className="greeting-section__weather-widget" />
+      <HelloWidget
+        name={name}
+        time={time}
+        className="greeting-section__hello-widget"
+      />
+      <WeatherWidget time={time} className="greeting-section__weather-widget" />
     </div>
   );
 };
