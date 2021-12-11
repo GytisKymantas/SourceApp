9c44df9549a6a197eddceb5f01857d1bfe1da72d
Vitoldas <vitoldascebatorius@gmail.com>
2021-12-11 21:56:45 +0200

    update prop to prop destructuring

--- src/components/atoms/NewsFeedCard/NewsFeedCard.jsx
+++ src/components/atoms/NewsFeedCard/NewsFeedCard.jsx
@@ -6,81 +6,85 @@ import vectorComment from "../../../assets/vectorComment.svg";
 import vectorLikes from "../../../assets/vectorLikes.svg";
 import "../NewsFeedCard/news-feed-card.scss";
 
-const NewsFeedItem = ({
-  userName,
-  userImage,
-  postVideo,
-  postLocation,
-  postDate,
-  postLikes,
-  postComments,
-}) => (
-  <div className="card">
-    <div className="card-header__wrapper">
-      <div className="card__title">
-        <img src={userImage} alt="your profile" />
-        <p className="card--name">{userName}</p>
-      </div>
-      <div className="card__timestamp">
-        <p>{postLocation}</p>
-        <p className="card--time">{postDate}</p>
-      </div>
-    </div>
-    {postVideo ? (
-      <div className="card__content-video">
-        <img className="vector__one" src={vectorPlay} alt="" />
-        <img className="vector__two" src={vectorOval} alt="" />
-      </div>
-    ) : (
-      <div className="card__content-image">{userImage}</div>
-    )}
-    <div className="actions">
-      <button className="likes">
-        <div className="likes__outline">
-          <img src={vectorLikes} alt="like here" />
+export const NewsFeedItem = (props) => {
+  const {
+    userName,
+    userImage,
+    postVideo,
+    postLocation,
+    postDate,
+    postLikes,
+    postComments,
+  } = props;
+
+  return (
+    <div className="card">
+      <div className="card-header__wrapper">
+        <div className="card__title">
+          <img src={userImage} alt="your profile" />
+          <p className="card--name">{userName}</p>
         </div>
-        <div className="likes__body">{postLikes}</div>
-      </button>
-      <button className="comments">
+        <div className="card__timestamp">
+          <p>{postLocation}</p>
+          <p className="card--time">{postDate}</p>
+        </div>
+      </div>
+      {postVideo ? (
+        <div className="card__content-video">
+          <img className="vector__one" src={vectorPlay} alt="" />
+          <img className="vector__two" src={vectorOval} alt="" />
+        </div>
+      ) : (
+        <div className="card__content-image">{userImage}</div>
+      )}
+      <div className="actions">
+        <button className="likes">
+          <div className="likes__outline">
+            <img src={vectorLikes} alt="like here" />
+          </div>
+          <div className="likes__body">{postLikes}</div>
+        </button>
+        <button className="comments">
+          <div>
+            <img src={vectorComment} alt="leave comment here" />
+          </div>
+          <div className="comments__body">{postComments}</div>
+        </button>
+      </div>
+      <div className="comment__wrapper">
+        <span className="divider_top"></span>
         <div>
-          <img src={vectorComment} alt="leave comment here" />
-        </div>
-        <div className="comments__body">{postComments}</div>
-      </button>
-    </div>
-    <div className="comment__wrapper">
-      <span className="divider_top"></span>
-      <div>
-        <div className="comments__rows">
-          <div className="comments__box_one">
-            <div className="comments__header">
-              <div className="comments__name">{userName}</div>
-              <div className="comments__timestamp">{postDate}</div>
+          <div className="comments__rows">
+            <div className="comments__box_one">
+              <div className="comments__header">
+                <div className="comments__name">{userName}</div>
+                <div className="comments__timestamp">{postDate}</div>
+              </div>
+              <div className="comments--content"></div>
             </div>
-            <div className="comments--content"></div>
-          </div>
-          <div className="comments__box_two">
-            <div className="comments_header__two">
-              <div className="comments__name__two">{userName}</div>
-              <div className="comments_timestamp__two">{postDate}</div>
+            <div className="comments__box_two">
+              <div className="comments_header__two">
+                <div className="comments__name__two">{userName}</div>
+                <div className="comments_timestamp__two">{postDate}</div>
+              </div>
+              <div className="comments--content__two"></div>
             </div>
-            <div className="comments--content__two"></div>
           </div>
         </div>
-      </div>
-      <span className="divider_bottom"></span>
-      <div className="comment_input">
-        <img src={userImage} alt="your profile icon" />
-        <input
-          type="text"
-          placeholder="Leave a comment..."
-          className="input_leave_comment"
-        />
-        <button className="button__post">Post</button>
+        <span className="divider_bottom"></span>
+        <div className="comment_input">
+          <img src={userImage} alt="your profile icon" />
+          <input
+            type="text"
+            placeholder="Leave a comment..."
+            className="input_leave_comment"
+          />
+          <button className="button__post">Post</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 NewsFeedItem.propTypes = {
   userName: PropTypes.string,
@@ -95,5 +99,3 @@ NewsFeedItem.propTypes = {
     postDate: PropTypes.string,
   }),
 };
-
-export default NewsFeedItem;
