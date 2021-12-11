cf6abb9b981da9de86c55f5c786eb22a4cdbaab9
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-11 19:44:19 +0200

    add card decorations, text and style these elements

--- src/components/atoms/EventCard/EventCard.jsx
+++ src/components/atoms/EventCard/EventCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Avatar } from "../Avatar/Avatar";
+import { ReactComponent as EventCardGraphics } from "../../../assets/eventCardGraphics.svg";
 import "./event-card.scss";
 
 export const EventCard = () => {
@@ -8,6 +9,16 @@ export const EventCard = () => {
       <div className="card">
         <div className="card__image">
           <Avatar hasIcon={false} />
+        </div>
+        <EventCardGraphics className="card__details-graphics" />
+
+        <div className="card__details">
+          <div className="card__details-name">Firstname Lastname</div>
+          {/* prideti kad celebrates today */}
+          <div className="card__details-birthday">
+            Celebrated a birthday on{" "}
+          </div>
+          <div className="card__details-wish">Send a wish!</div>
         </div>
       </div>
     </>
