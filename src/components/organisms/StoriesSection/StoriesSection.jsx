a15a9f7200322de343f0bb708989a220768796ab
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-13 17:47:29 +0200

    pass data to event card component

--- src/components/organisms/StoriesSection/StoriesSection.jsx
+++ src/components/organisms/StoriesSection/StoriesSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { EventCard } from "components/atoms/EventCard/EventCard.jsx";
+import { EventCard } from "components/molecules/EventCard/EventCard.jsx";
 export const StoriesSection = ({ data }) => {
   const storiesData = data?.stories;
   const Birthdays = storiesData?.filter((x) => x.type === "birthday");
