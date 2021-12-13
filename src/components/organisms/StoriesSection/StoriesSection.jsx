90129336079a3ba04edc0b48b1344aa26b52cc31
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-13 14:01:54 +0200

    solve merge conflict in package-lock.json

--- src/components/organisms/StoriesSection/StoriesSection.jsx
+++ src/components/organisms/StoriesSection/StoriesSection.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import { EventCard } from "components/atoms/EventCard/EventCard.jsx";
 export const StoriesSection = ({ data }) => {
-  return <div>Stories section </div>;
+  const storiesData = data?.stories;
+  const Birthdays = storiesData?.filter((x) => x.type === "birthday");
+
+  return (
+    <div className="section">
+      <EventCard data={Birthdays} />
+    </div>
+  );
 };
 
 StoriesSection.propTypes = {
