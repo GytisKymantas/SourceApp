287168864cc215d1eff28a9389383978b32da42f
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2021-12-13 21:45:40 +0200

    .

--- src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
+++ src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./news-feed-section.scss";
+import { EventCard } from "components/molecules/EventCard/EventCard";
 
 export const NewsFeedSection = ({ data }) => {
+  const storiesData = data?.stories;
+  const Birthdays = storiesData?.filter((x) => x.type === "birthday");
+
   return (
     <div>
       <h2 className="news-section__header">News and stories</h2>
@@ -10,6 +14,9 @@ export const NewsFeedSection = ({ data }) => {
         <div className="news-section__container-side">item</div>
         <div className="news-section__container-middle">item</div>
         <div className="news-section__container-side">item</div>
+      </div>
+      <div className="section">
+        <EventCard data={Birthdays} />
       </div>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { NewsFeedSection } from "components/organisms/NewsFeedSection/NewsFeedSection";
 import { ViewEatOutCard } from "components/molecules/ViewEatOutCard/ViewEatOutCard";
-import { EventCard } from "components/atoms/EventCard/EventCard";
 
 export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
@@ -34,7 +33,6 @@ export const Dashboard = (data) => {
       <HelloWidget name={firstName} />
       <ReservationsSection reservationData={reservationData} />
       {storiesData && <NewsFeedSection data={storiesData} />}
-      <EventCard />
       <ViewEatOutCard />
     </MainLayout>
   );
