f16964c226a38a9505f83632f33ea4a3f7ff3305
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-15 23:40:46 +0200

    add empty line after imports and rename variable

--- src/components/organisms/StoriesSection/StoriesSection.jsx
+++ src/components/organisms/StoriesSection/StoriesSection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { EventCard } from "components/molecules/EventCard/EventCard.jsx";
+
 export const StoriesSection = ({ data }) => {
   const storiesData = data?.stories;
-  const Birthdays = storiesData?.filter((x) => x.type === "birthday");
+  const birthdays = storiesData?.filter((x) => x.type === "birthday");
 
   return (
     <div className="section">
-      {Birthdays && <EventCard data={Birthdays[0]} />}
+      {birthdays && <EventCard data={birthdays[0]} />}
     </div>
   );
 };
