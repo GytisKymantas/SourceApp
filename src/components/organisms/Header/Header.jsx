3c63f59ab7117570d47f28ed1387247ff9c66789
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-12-15 12:28:53 +0200

    Add conditonal logo rendering

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { ReactComponent as Logo } from "assets/rocketLogo.svg";
+import useReactSimpleMatchMedia from "react-simple-matchmedia";
+import { ReactComponent as RocketLogo } from "assets/rocketLogo.svg";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 import "./header.scss";
 import { ProfileWidget } from "../../molecules/ProfileWidget/ProfileWidget";
@@ -7,18 +9,20 @@ import { Navigation } from "../../molecules/Navigation/Navigation";
 
 const logoPath = "/dashboard";
 
-export const Header = () => (
-  <header className="header">
-    <div className="header__logo">
-      <a href={logoPath}>
-        <Logo />
-      </a>
-    </div>
-    <div className="header__navigation">
-      <Navigation />
-    </div>
-    <div className="header__profile-widget">
-      <ProfileWidget />
-    </div>
-  </header>
-);
+export const Header = () => {
+  const matched = useReactSimpleMatchMedia("(max-width: 768px)");
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <a href={logoPath}>{matched ? <RocketLogo /> : <Logo />}</a>
+      </div>
+      <div className="header__navigation">
+        <Navigation />
+      </div>
+      <div className="header__profile-widget">
+        <ProfileWidget />
+      </div>
+    </header>
+  );
+};
