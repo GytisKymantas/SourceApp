5a642bb62b2f1f5f2bc501790e107a3543819170
gytis <Gytis.kymantas@yahoo.com>
2021-12-15 20:36:47 +0200

    change dashboard to render section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,15 +4,31 @@ import { ReservationsSection } from "components/molecules/ReservationsSection/Re
 import React from "react";
 import { useState, useEffect } from "react";
 import { StoriesSection } from "components/organisms/StoriesSection/StoriesSection";
+import { RestaurantCard } from "components/atoms/RestaurantCard/RestaurantCard";
 import { ViewEatOutCard } from "components/molecules/ViewEatOutCard/ViewEatOutCard";
 
 export const Dashboard = (data) => {
   const userData = data.data?.userData[0];
   const firstName = userData?.userName.split(" ")[0];
   const reservationData = userData?.reservations;
+  // const restaurantList = restaurantData?.restaurants[0];
   const [storiesData, setStoriesData] = useState();
+  const [restaurantData, setRestaurantData] = useState();
 
   useEffect(() => {
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/restaurants.json"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setRestaurantData(result);
+        },
+
+        (error) => {
+          // handle error here
+        }
+      );
     fetch(
       "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/stories.json"
     )
@@ -33,6 +49,21 @@ export const Dashboard = (data) => {
       <HelloWidget name={firstName} />
       <ReservationsSection reservationData={reservationData} />
       {storiesData && <StoriesSection data={storiesData} />}
+      {restaurantData && (
+        <RestaurantCard
+          displayFullCard={true}
+          numberOfCheckIns={restaurantData?.restaurants[0].checkIns}
+          restaurantName={restaurantData?.restaurants[0].name}
+          restaurantWebsite={restaurantData?.restaurants[0].website.slice(7)}
+          restaurantAddress={restaurantData?.restaurants[0].location.address}
+          restaurantOpeningHours={
+            restaurantData?.restaurants[0].openingHours[0].hours
+          }
+          restaurantDescription={restaurantData?.restaurants[0].description}
+          restaurantCategories={restaurantData?.restaurants[0].categories}
+          restaurantImage={restaurantData?.restaurants[0].image}
+        />
+      )}
       <ViewEatOutCard />
     </MainLayout>
   );
