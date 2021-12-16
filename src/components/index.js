0e12ffe1ffc7187fd092d87bbdcecb32cf766857
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-16 13:41:12 +0200

    add exports to main index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ export * from "./atoms/Avatar/index";
 export * from "./atoms/DropDown/index";
 export * from "./atoms/StartingPageLayout/index";
 export * from "./atoms/MenuItem/index";
-
+export * from "./atoms/HelloWidget/index";
+export * from "./atoms/WeatherWidget/index";
 // ----- MOLECULES ----- //
 export * from "./molecules/ProfileWidget/index";
 export * from "./molecules/Navigation/index";
+export * from "./molecules/GreetingSection/index";
