06cba4492b08b1bfae38b19f4000dadd9cdcf454
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-16 11:57:44 +0200

    move classNames before return statement

--- src/components/atoms/Avatar/Avatar.jsx
+++ src/components/atoms/Avatar/Avatar.jsx
@@ -5,17 +5,18 @@ import classNames from "classnames";
 
 import "./avatar.scss";
 
-export const Avatar = ({ imageSource, hasIcon, isClickable, isLarge }) => (
-  <div
-    className={classNames("avatar", {
-      "avatar--large": isLarge,
-      "avatar--clickable": isClickable,
-    })}
-  >
-    <img className="avatar__picture" src={imageSource} alt="profile" />
-    {hasIcon && <HeartLogo className="avatar__logo" />}
-  </div>
-);
+export const Avatar = ({ imageSource, hasIcon, isClickable, isLarge }) => {
+  const className = classNames("avatar", {
+    "avatar--large": isLarge,
+    "avatar--clickable": isClickable,
+  });
+  return (
+    <div className={className}>
+      <img className="avatar__picture" src={imageSource} alt="profile" />
+      {hasIcon && <HeartLogo className="avatar__logo" />}
+    </div>
+  );
+};
 
 Avatar.defaultProps = {
   imageSource:
