99537395ea4448f9c2fc986a22c42356de3f0ffe
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-17 15:48:05 +0200

    remove unused return statement

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -3,16 +3,14 @@ import PropTypes from "prop-types";
 import "./menu-item.scss";
 import { NavLink } from "react-router-dom";
 
-export const MenuItem = ({ icon, name, link }) => {
-  return (
-    <NavLink to={link} className="menu-item" activeclassname="active">
-      <div className="menu-item__content">
-        <div className="menu-item__icon">{icon}</div>
-        <div className="menu-item__name">{name}</div>
-      </div>
-    </NavLink>
-  );
-};
+export const MenuItem = ({ icon, name, link }) => (
+  <NavLink to={link} className="menu-item" activeclassname="active">
+    <div className="menu-item__content">
+      <div className="menu-item__icon">{icon}</div>
+      <div className="menu-item__name">{name}</div>
+    </div>
+  </NavLink>
+);
 MenuItem.propTypes = {
   icon: PropTypes.node,
   name: PropTypes.string,
