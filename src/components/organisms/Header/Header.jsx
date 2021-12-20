cdf5cfb237c1b7e0c599d1a2dafb3acfe9067c46
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-20 17:22:54 +0200

    undo changes

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Logo } from "assets/logo.svg";
+import { ReactComponent as Logo } from "assets/rocketLogo.svg";
 
 import "./header.scss";
 import { ProfileWidget } from "../../molecules/ProfileWidget/ProfileWidget";
