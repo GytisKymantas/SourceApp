f6bc68ee0013fc5610c6c151785c5c154fa92f04
Vilija Martinaitytė <martinaitytev@gmail.com>
2021-12-20 17:17:57 +0200

    undo changes

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { ReactComponent as RocketLogo } from "assets/rocketLogo.svg";
+import React from "react";
 import { ReactComponent as Logo } from "assets/logo.svg";
 
 import "./header.scss";
@@ -8,24 +7,12 @@ import { Navigation } from "../../molecules/Navigation/Navigation";
 
 const logoPath = "/dashboard";
 
-export const Header = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const breakPoint = 768;
-
-  useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => window.removeEventListener("resize", handleWindowResize);
-  }, []);
-
-  return (
-    <header className="header">
-      <a className="header__logo" href={logoPath}>
-        {width > breakPoint ? <Logo /> : <RocketLogo />}
-      </a>
-      <Navigation className="header__navigation" />
-      <ProfileWidget className="header__profile-widget" />
-    </header>
-  );
-};
+export const Header = () => (
+  <header className="header">
+    <a href={logoPath}>
+      <Logo />
+    </a>
+    <Navigation />
+    <ProfileWidget />
+  </header>
+);
