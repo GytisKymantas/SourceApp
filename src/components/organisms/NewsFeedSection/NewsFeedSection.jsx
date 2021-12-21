35e5cdeba14becd353d6e822de5514ce49fb10fd
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2021-12-21 14:42:17 +0200

    Add starter code

--- src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
+++ src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
@@ -8,15 +8,33 @@ export const NewsFeedSection = ({ data }) => {
   const Birthdays = storiesData?.filter((x) => x.type === "birthday");
 
   return (
-    <div>
+    <div className="news-section-container">
       <h2 className="news-section__header">News and stories</h2>
       <div className="news-section">
-        <div className="news-section__container-side">item</div>
-        <div className="news-section__container-middle">item</div>
-        <div className="news-section__container-side">item</div>
-      </div>
-      <div className="section">
-        <EventCard data={Birthdays} />
+        <div className="news-section__column">
+          <div className="news-section__column__event-card">
+            <EventCard data={Birthdays} />
+          </div>
+          <div className="news-section__column__news-post">
+            <b>Vitoldo kortelė</b>
+          </div>
+        </div>
+        <div className="news-section__column">
+          <div className="news-section__column__news-post">
+            <b>Vitoldo kortelė</b>
+          </div>
+          <div className="news-section__column__event-card">
+            <EventCard data={Birthdays} />
+          </div>
+        </div>
+        <div className="news-section__column">
+          <div className="news-section__column__event-card">
+            <EventCard data={Birthdays} />
+          </div>
+          <div className="news-section__column__news-post">
+            <b>Vitoldo kortelė</b>
+          </div>
+        </div>
       </div>
     </div>
   );
