1fac7715065bc16712854114d72a7473dddea2c1
gytis <Gytis.kymantas@yahoo.com>
2021-12-22 10:53:15 +0200

    first commit

---
+++ src/components/atoms/EatOutSliderCard/EatOutSlider.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import CassaDellaPasta from "../../../assets/CassaDellaPasta.svg";
+import SliderBulletPoint from "../../../assets/SliderBulletPoint.svg";
+import SliderBulletPointSelected from "../../../assets/SliderBulletPointSelected.svg";
+import { Button } from "components/atoms/Button/Button";
+import PlaceholderNav from "../../../assets/PlaceholderNav.svg";
+
+// import PropTypes from "prop-types";
+
+import "./eat-out-slider.scss";
+
+const EatOutSlider = () => {
+  return (
+    <div className="slider__container">
+      <div className="slider__image-wrapper">
+        <img src={CassaDellaPasta} className="slider-image" alt="salad bowl" />
+      </div>
+      <div className="slider-text__wrapper">
+        <div className="top-navigation">
+          <div className="bullets">
+            <img src={SliderBulletPoint} alt="bullet point" />
+            <img src={SliderBulletPoint} alt="bullet point" />
+            <img src={SliderBulletPointSelected} alt="selected point" />
+            <img src={SliderBulletPoint} alt="bullet point" />
+            <img src={SliderBulletPoint} alt="bullet point" />
+          </div>
+          <div className="placeholder-nav">
+            <img src={PlaceholderNav} alt="placeholder" />
+          </div>
+        </div>
+        <span className="text-slogan">Feel the taste of italy</span>
+        <h2>Casa Della Pasta</h2>
+        <p>
+          Casa della Pasta was the first restaurant in Kaunas cooking pizza in a
+          traditional wood-oven; a restaurant capable of satisfying more than
+          3000 customers...
+        </p>
+        <div className="btn-container">
+          <Button label={"LEARN MORE"} isLarge className="LargeBtn" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EatOutSlider;
