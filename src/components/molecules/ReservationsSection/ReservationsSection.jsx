a993c2696ba9ddfb0ceae0eebe787738c2a0d298
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2021-12-23 20:06:49 +0200

    Add clickable reservation cards and fix card ordering to fit design requirements.

--- src/components/molecules/ReservationsSection/ReservationsSection.jsx
+++ src/components/molecules/ReservationsSection/ReservationsSection.jsx
@@ -5,9 +5,17 @@ import Book from "../../../assets/book.svg";
 import Phone from "../../../assets/phone.svg";
 import "./reservations-section.scss";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export const ReservationsSection = ({ reservationData }) => {
   const reservationArray = reservationData && Object.entries(reservationData);
+  const correctNaming = ["rooms", "books", "devices"];
+
+  if (reservationArray !== undefined) {
+    for (let i = 0; i < 3; i++) {
+      reservationArray[i][0] = correctNaming[i];
+    }
+  }
 
   const nameMatcher = (name) => {
     switch (name) {
@@ -39,12 +47,26 @@ export const ReservationsSection = ({ reservationData }) => {
         {reservationArray
           ? reservationArray.map((obj, index) => {
               return (
-                <ReservationsItem
-                  reservationName={nameMatcher(obj[0])}
-                  reservationNumber={obj[1].length}
-                  reservationImage={imageMatcher(obj[0])}
+                <Link
                   key={index}
-                />
+                  className="reservations-section__element-container--link"
+                  to={
+                    obj[0] === "books"
+                      ? "/dashboard/reservations/books"
+                      : obj[0] === "devices"
+                      ? "/dashboard/reservations/devices"
+                      : obj[0] === "rooms"
+                      ? "/dashboard/reservations/meetingrooms"
+                      : "/"
+                  }
+                >
+                  <ReservationsItem
+                    reservationName={nameMatcher(obj[0])}
+                    reservationNumber={obj[1].length}
+                    reservationImage={imageMatcher(obj[0])}
+                    key={index}
+                  />
+                </Link>
               );
             })
           : null}
