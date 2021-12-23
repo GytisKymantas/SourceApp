28092194087b263e65d298c80179de080aa5216d
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2021-12-23 18:48:21 +0200

    Fix card naming.

--- src/components/molecules/ReservationsSection/ReservationsSection.jsx
+++ src/components/molecules/ReservationsSection/ReservationsSection.jsx
@@ -12,7 +12,9 @@ export const ReservationsSection = ({ reservationData }) => {
   const nameMatcher = (name) => {
     switch (name) {
       case "books":
-        return "Meeting Rooms";
+        return "Books";
+      case "rooms":
+        return "Meeting rooms";
       default:
         return name;
     }
