14e9524d61ced2736908981151d9477f75c8b72c
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-01-04 17:37:00 +0200

    Complete 3 flexbox column approach

--- src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
+++ src/components/organisms/NewsFeedSection/NewsFeedSection.jsx
@@ -5,92 +5,39 @@ import { EventCard } from "components/molecules/EventCard/EventCard";
 
 export const NewsFeedSection = ({ data }) => {
   const storiesData = data?.stories;
-  const birthdays = storiesData?.filter((x) => x.type === "birthday");
-  const posts = storiesData?.filter((x) => x.type === "post");
-  const videos = storiesData?.filter((x) => x.type === "video");
+  const storiesAmount = storiesData?.length;
+  const storiesPerColumn = Math.ceil(storiesAmount / 3);
+  const columnData = [
+    storiesData?.slice(0, storiesPerColumn),
+    storiesData?.slice(storiesPerColumn, storiesPerColumn * 2),
+    storiesData?.slice(storiesPerColumn * 2),
+  ];
 
   return (
     <div className="news-section-container">
       <h2 className="news-section__header">News and stories</h2>
       <div className="news-section">
-        <div className="news-section__column">
-          {storiesData?.slice(0, 6).map((item, index) => {
-            if (item.type === "birthday") {
+        {columnData?.map((col, index) => (
+          <div className="news-section__column" key={index}>
+            {col.map((card) => {
+              if (card.type === "birthday") {
+                return (
+                  <div
+                    className="news-section__column__event-card"
+                    key={card.id}
+                  >
+                    {card && <EventCard data={card} />}
+                  </div>
+                );
+              }
               return (
-                <div className="news-section__column__event-card">
-                  {birthdays[index] && <EventCard data={birthdays[index]} />}
+                <div className="news-section__column__news-post" key={card.id}>
+                  {card.userName}
                 </div>
               );
-            }
-            if (item.type === "post") {
-              return (
-                <div className="news-section__column__news-post">
-                  {posts[index]?.userName}
-                </div>
-              );
-            }
-            if (item.type === "video") {
-              return (
-                <div className="news-section__column__news-post">
-                  {videos[index]?.userName}
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-        <div className="news-section__column">
-          {storiesData?.slice(6, 11).map((item, index) => {
-            if (item.type === "birthday") {
-              return (
-                <div className="news-section__column__event-card">
-                  {birthdays[index] && <EventCard data={birthdays[index]} />}
-                </div>
-              );
-            }
-            if (item.type === "post") {
-              return (
-                <div className="news-section__column__news-post">
-                  {posts[index]?.userName}
-                </div>
-              );
-            }
-            if (item.type === "video") {
-              return (
-                <div className="news-section__column__news-post">
-                  {videos[index]?.userName}
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-        <div className="news-section__column">
-          {storiesData?.slice(11, 16).map((item, index) => {
-            if (item.type === "birthday") {
-              return (
-                <div className="news-section__column__event-card">
-                  {birthdays[index] && <EventCard data={birthdays[index]} />}
-                </div>
-              );
-            }
-            if (item.type === "post") {
-              return (
-                <div className="news-section__column__news-post">
-                  {posts[index]?.userName}
-                </div>
-              );
-            }
-            if (item.type === "video") {
-              return (
-                <div className="news-section__column__news-post">
-                  {videos[index]?.userName}
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
