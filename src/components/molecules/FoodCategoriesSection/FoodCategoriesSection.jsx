d02bb19b0f16895bcf12d4016d0a6eb3f7cb0d94
Vilija Martinaitytė <martinaitytev@gmail.com>
2022-01-06 17:07:58 +0200

    implement maping through data, passing it to items, add styles for section

--- src/components/molecules/FoodCategoriesSection/FoodCategoriesSection.jsx
+++ src/components/molecules/FoodCategoriesSection/FoodCategoriesSection.jsx
@@ -3,36 +3,21 @@ import "./food-categories-section.scss";
 import { Link } from "react-router-dom";
 
 import { CategoryItem } from "components/atoms/CategoryItem/CategoryItem";
-// import Barbecue from "../../../assets/barbecue.svg";
 import Bread from "../../../assets/bread.svg";
-// import Breakfast from "../../../assets/breakfast.svg";
+import Breakfast from "../../../assets/breakfast.svg";
 import Burger from "../../../assets/burger.svg";
-// import Cake from "../../../assets/cake.svg";
-// import Cheese from "../../../assets/cheese.svg";
-// import Dango from "../../../assets/dango.svg";
-// import Donut from "../../../assets/donut.svg";
-// import Dumpling from "../../../assets/dumpling.svg";
-// import FrenchFries from "../../../assets/frenchFries.svg";
-// import FriedChicken from "../../../assets/friedChicken.svg";
-// import FriedEgg from "../../../assets/friedEgg.svg";
-// import HotDog from "../../../assets/hotDog.svg";
-// import IceCream from "../../../assets/iceCream.svg";
-// import Jam from "../../../assets/jam.svg";
-// import Kebab from "../../../assets/kebab.svg";
-// import Onigiri from "../../../assets/onigiri.svg";
-// import Pancake from "../../../assets/pancake.svg";
-// import Pizza from "../../../assets/pizza.svg";
-// import Popsicle from "../../../assets/popsicle.svg";
-// import Ramen from "../../../assets/ramen.svg";
-import Salads from "../../../assets/salad.svg";
-// import Sandwich from "../../../assets/sandwich.svg";
-// import Sausage from "../../../assets/sausage.svg";
-// import Shrimp from "../../../assets/shrimp.svg";
-// import Soup from "../../../assets/soup.svg";
-import Steak from "../../../assets/steak.svg";
-// import SushiMaki from "../../../assets/sushiMaki.svg";
-// import SushiNigiri from "../../../assets/sushiNigiri.svg";
-// import Tempura from "../../../assets/tempura.svg";
+import Cake from "../../../assets/cake.svg";
+import Donut from "../../../assets/donut.svg";
+import HotDog from "../../../assets/hotDog.svg";
+import Kebab from "../../../assets/kebab.svg";
+import Pancake from "../../../assets/pancake.svg";
+import Pizza from "../../../assets/pizza.svg";
+import Ramen from "../../../assets/ramen.svg";
+import Salad from "../../../assets/salad.svg";
+import Sandwich from "../../../assets/sandwich.svg";
+import Soup from "../../../assets/soup.svg";
+import Barbecue from "../../../assets/barbecue.svg";
+import SushiMaki from "../../../assets/sushiMaki.svg";
 import PropTypes from "prop-types";
 
 export const FoodCategoriesSection = ({ restaurantsData }) => {
@@ -48,7 +33,6 @@ export const FoodCategoriesSection = ({ restaurantsData }) => {
   }
 
   const counts = {};
-
   for (const num of categories) {
     counts[num] = counts[num] ? counts[num] + 1 : 1;
   }
@@ -60,8 +44,6 @@ export const FoodCategoriesSection = ({ restaurantsData }) => {
 
   const nameMatcher = (name) => {
     switch (name) {
-      case "rooms":
-        return "Meeting Rooms";
       default:
         return name;
     }
@@ -70,34 +52,60 @@ export const FoodCategoriesSection = ({ restaurantsData }) => {
     switch (image) {
       case "burger":
         return Burger;
-      case "salad":
-        return Salads;
+      case "salads":
+        return Salad;
       case "grill":
-        return Steak;
+        return Barbecue;
+      case "pizza":
+        return Pizza;
+      case "pancakes":
+        return Pancake;
+      case "soups":
+        return Soup;
+      case "sweets":
+        return Cake;
+      case "breakfast":
+        return Breakfast;
+      case "ramen":
+        return Ramen;
+      case "sandwich":
+        return Sandwich;
+      case "kebab":
+        return Kebab;
+      case "sushi":
+        return SushiMaki;
+      case "hot dogs":
+        return HotDog;
+      case "donuts":
+        return Donut;
       default:
         return Bread;
     }
   };
   return (
-    <div>
-      {counts
-        ? Object.entries(counts).map((name, index) => {
-            return (
-              <Link
-                key={index}
-                className="food-categories-section__element"
-                to={getPath(name[0].toLowerCase())}
-              >
-                <CategoryItem
-                  categoryName={nameMatcher(name[0])}
-                  categoryInfo={name[1] > 1 ? `${name[1]} PLACES` : "PLACE"}
-                  categoryImage={imageMatcher(name[0])}
+    <div className="food-categories-section">
+      <h2 className="food-categories-section__header">Categories</h2>
+      <div className="food-categories-section__element-container">
+        {counts
+          ? Object.entries(counts).map((name, index) => {
+              return (
+                <Link
                   key={index}
-                />
-              </Link>
-            );
-          })
-        : null}
+                  className="food-categories-section__element"
+                  to={getPath(name[0].toLowerCase())}
+                >
+                  <CategoryItem
+                    categoryName={nameMatcher(name[0])}
+                    categoryInfo={`${name[1]}
+                    ${name[1] % 10 === 1 ? "PLACE" : "PLACES"}`}
+                    categoryImage={imageMatcher(name[0].toLowerCase())}
+                    key={index}
+                  />
+                </Link>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
