39a6a8b74720ff862d805708ba2b14962500b555
Vilija Martinaitytė <martinaitytev@gmail.com>
2022-01-07 14:27:43 +0200

    remove unused event card call

--- src/components/organisms/StoriesSection/StoriesSection.jsx
+++ src/components/organisms/StoriesSection/StoriesSection.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { EventCard } from "components/molecules/EventCard/EventCard.jsx";
-import { NewsFeedCard } from "../../molecules/NewsFeedCard/NewsFeedCard.jsx";
 
 export const StoriesSection = ({ storiesData, userData }) => {
   const stories = storiesData?.stories;
   const birthdays = stories?.filter((story) => story.type === "birthday");
-  const postNews = stories?.filter(
-    (x) => x.type === "post" || ("video" && x.type !== "birthday")
-  );
-  const userImage = userData?.userImage;
 
   return (
     <div className="section">
       {birthdays && (
         <EventCard storiesData={birthdays[0]} userData={userData} />
       )}
-      <NewsFeedCard postData={postNews} currentUserImage={userImage} />
     </div>
   );
 };
