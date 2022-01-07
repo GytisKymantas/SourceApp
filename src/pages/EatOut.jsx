edd47b5018f125c3d90c098dcd195f3dd5c6c118
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-01-07 17:02:04 +0200

    Create an array of 6 most recently opened restaurants

--- src/pages/EatOut.jsx
+++ src/pages/EatOut.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const EatOut = ({ data }) => {
-  const restaurantsData = data;
+  const restaurantsData = data?.restaurants;
+
+  // eslint-disable-next-line no-unused-vars
+  const sortedByDate = []
+    .concat(restaurantsData)
+    .sort((a, b) => (a.createdDate > b.createdDate ? -1 : 1))
+    .slice(0, 6);
 
   return (
     <MainLayout>
