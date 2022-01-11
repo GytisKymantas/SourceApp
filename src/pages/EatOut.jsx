f3f5cb3732fb9a75297eac75d4c74fd1a196cc61
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2022-01-11 16:49:55 +0200

    Adds Carousel to Eat-out page

--- src/pages/EatOut.jsx
+++ src/pages/EatOut.jsx
@@ -1,13 +1,49 @@
 import { MainLayout } from "components/layouts/MainLayout/MainLayout";
 import React from "react";
 import PropTypes from "prop-types";
+// use real data here and remove this file later
+import { restaurantsData } from "./data";
+import { RestaurantCard } from "components/molecules/RestaurantCard/RestaurantCard";
+import { Carousel } from "components/molecules/Carousel/Carousel";
+
+import "./eat-out.scss";
 
 export const EatOut = ({ data }) => {
-  const restaurantsData = data;
+  // use real data here and remove this file later
+  // const restaurantsData = data;
 
   return (
     <MainLayout>
       <h1>EatOut</h1>
+      <div className="eat-out__carousel-container">
+        <Carousel title="New places">
+          {restaurantsData?.map((restaurant) => (
+            <div
+              key={restaurant.id}
+              className="eat-out__carousel-container--inside"
+            >
+              <RestaurantCard
+                displayFullCard
+                numberOfCheckIns={restaurant.checkIns}
+                restaurantName={restaurant.name}
+                restaurantWebsite={restaurant.website.replace(
+                  /(^\w:|^)\/\//,
+                  ""
+                )}
+                restaurantAddress={restaurant.location.address}
+                restaurantOpeningHours={restaurant.openingHours[0].hours}
+                restaurantDescription={restaurant.description}
+                restaurantCategories={restaurant.categories}
+                restaurantImage={restaurant.image}
+                // not sure if this counting is needed with new data
+                restaurantAverageRating={restaurant.reviews.map(
+                  (review) => review.rating
+                )}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </MainLayout>
   );
 };
