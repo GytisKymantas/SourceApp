f265c043f8f7baf44844f5b9957e8f00a48fc0a9
Vitoldas <vitoldascebatorius@gmail.com>
2022-01-11 20:20:39 +0200

    Updated slider arrow functionality

--- src/components/atoms/SliderArrows/SliderArrows.jsx
+++ src/components/atoms/SliderArrows/SliderArrows.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-// import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { ReactComponent as SliderArrowLeft } from "../../../assets/sliderArrowLeft.svg";
 import { ReactComponent as SliderArrowRight } from "../../../assets/sliderArrowRight.svg";
 import "./sliderArrows.scss";
 
-export const SliderArrows = () => {
+export const SliderArrows = (props) => {
+  const { onClickSlider } = props;
+
+  const handleClick = (onClickSlider) => {
+    onClickSlider = props;
+  };
+
   return (
     <div className="container__slider--arrows">
       <button className="btn__left">
-        <SliderArrowLeft className="arrow__left" />
+        <SliderArrowLeft
+          className="arrow__left"
+          onClick={() => {
+            handleClick(onClickSlider + " Arrow Left");
+          }}
+        />
       </button>
       <button className="btn__right">
-        <SliderArrowRight className="arrow__right" />
+        <SliderArrowRight
+          className="arrow__right"
+          onClick={() => {
+            handleClick(onClickSlider + " Arrow Right");
+          }}
+        />
       </button>
     </div>
   );
 };
 
-// SliderArrows.propTypes = {
-
-// }
+SliderArrows.propTypes = {
+  onClickSlider: PropTypes.func,
+};
