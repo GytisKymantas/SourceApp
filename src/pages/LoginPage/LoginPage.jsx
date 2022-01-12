105fe1c6cd0eca0cdf02e0ba4f6847811b54f33e
Vitoldas <vitoldascebatorius@gmail.com>
2022-01-12 09:01:56 +0200

    updated reverted login changes

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -31,6 +31,7 @@ export const LoginPage = ({ setIsLoggedIn }) => {
       emailFromStorage,
       passwordFromStorage,
     } = retrieveFromSessionStorage();
+
     if (
       canSubmit &&
       email === emailFromStorage &&
