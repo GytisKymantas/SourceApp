a2a0d75db526a51dcd742d585ab18238da53fb61
Vitoldas <vitoldascebatorius@gmail.com>
2022-01-12 14:02:57 +0200

    updated files after newest changes

--- src/components/organisms/StoriesSection/StoriesSection.jsx
+++ src/components/organisms/StoriesSection/StoriesSection.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { EventCard } from "components/molecules/EventCard/EventCard.jsx";
-
+import { NewsFeedCard } from "../../molecules/NewsFeedCard/NewsFeedCard.jsx";
 export const StoriesSection = ({ storiesData, userData }) => {
   const stories = storiesData?.stories;
   const birthdays = stories?.filter((story) => story.type === "birthday");
-
+  const postNews = stories?.filter(
+    (postCard) =>
+      postCard.type === "post" || ("video" && postCard.type !== "birthday")
+  );
   return (
     <div className="section">
-      {birthdays && (
-        <EventCard storiesData={birthdays[0]} userData={userData} />
-      )}
+      {birthdays && <EventCard data={birthdays[0]} />}
+      <NewsFeedCard data={postNews[7]} userData={userData} />
     </div>
   );
 };
