a0d79bafe681e7fcbd74e7ee53bbf6f1ef72e68d
Vilija Martinaitytė <martinaitytev@gmail.com>
2022-01-13 13:27:53 +0200

    fix vieweatoutcard, restaurant card's styles, sections styles

--- src/components/molecules/ViewEatOutCard/ViewEatOutCard.jsx
+++ src/components/molecules/ViewEatOutCard/ViewEatOutCard.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../atoms/Button/Button";
-import LinesEllipsis from "react-lines-ellipsis";
-
 import "./view-eat-out-card.scss";
 
 export const ViewEatOutCard = () => {
@@ -12,14 +10,12 @@ export const ViewEatOutCard = () => {
     let path = `/dashboard/eatout`;
     navigate(path);
   };
-  const text =
-    "import LinesEllipsis from 'react-lines-ellipsis import LinesEllipsis from 'react-lines-ellipsis' import LinesEllipsis from 'react-lines-ellipsis'";
 
   return (
     <div className="view-eat-out-card">
       <div className="view-eat-out-card__content">
         <div className="view-eat-out-card__text">
-          <LinesEllipsis text={text} maxLine="3" basedOn="words" />{" "}
+          View all your favourite lunch spots and more
         </div>
         <Button label="browse list" onClick={routeChange} />
       </div>
