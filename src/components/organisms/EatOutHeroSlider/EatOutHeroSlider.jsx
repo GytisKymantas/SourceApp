fa7da4e3eef74f409e5516fa7a8c8392bdae1906
gytis <Gytis.kymantas@yahoo.com>
2022-01-14 17:43:50 +0200

    dev

---
+++ src/components/organisms/EatOutHeroSlider/EatOutHeroSlider.jsx
@@ -0,0 +1,55 @@
+import React, { useState, Fragment, useEffect } from "react";
+import EatOutSliderCard from "components/molecules/EatOutSliderCard/EatOutSliderCard/";
+import PropTypes from "prop-types";
+
+const EatOutHeroSlider = (data) => {
+  const restaurantsData = data.data;
+
+  const [index, setIndex] = useState(0);
+  const handleImageClick = () => {
+    setIndex(index === 4 ? index - 4 : index + 1);
+  };
+
+  const handleCard = (e) => {
+    e === "right"
+      ? setIndex(index === 4 ? 4 : index + 1)
+      : setIndex(index === 0 ? 0 : index - 1);
+  };
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => setIndex(index === 4 ? index - 4 : index + 1),
+      5000
+    );
+
+    return () => clearTimeout(timeout);
+  }, [index]);
+
+  return (
+    <Fragment>
+      {restaurantsData && (
+        <EatOutSliderCard
+          restaurantSloganProp={restaurantsData[index].slogan}
+          restaurantImageProp={restaurantsData[index].image}
+          restaurantDescriptionProp={restaurantsData[index].description}
+          restaurantNameProp={restaurantsData[index].name}
+          index={index}
+          handleCard={handleCard}
+          handleImageClick={handleImageClick}
+        />
+      )}
+    </Fragment>
+  );
+};
+
+EatOutHeroSlider.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      slogan: PropTypes.string,
+      image: PropTypes.string,
+      description: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+export default EatOutHeroSlider;
