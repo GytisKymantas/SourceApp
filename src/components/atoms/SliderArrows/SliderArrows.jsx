0fda55e54c174995176287d94932b7d4a5c208fd
gytis <Gytis.kymantas@yahoo.com>
2022-01-18 14:56:18 +0200

    comments fix

--- src/components/atoms/SliderArrows/SliderArrows.jsx
+++ src/components/atoms/SliderArrows/SliderArrows.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ReactComponent as SliderArrowLeft } from "../../../assets/sliderArrowLeft.svg";
 import { ReactComponent as SliderArrowRight } from "../../../assets/sliderArrowRight.svg";
-import "./sliderArrows.scss";
+import "./slider-arrows.scss";
 
 export const SliderArrows = ({ onClickSlider }) => (
   <div className="container__slider--arrows">
